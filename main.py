5e5082e56b130734271c273f7bca6fd7d29c5de4
Polly <polinayusupova08@gmail.com>
2024-03-11 18:42:02 +0300

    Напоминание о др пользователя

--- main.py
+++ main.py
@@ -2,31 +2,50 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import schedule
+from threading import Thread
+from time import sleep
 import telebot
 from telebot import types
 bot = telebot.TeleBot('6827864691:AAH2MPjAwSdaQctyiic5Z2Nbo30AQ8rxMl8')
 date_of_bd = None
-user_id_tg = None
+chat_id_tg = None
 name = None
 phone = None
 email = None
 information = None
 message_chat_id = None
 import psycopg2
-connection = psycopg2.connect(dbname='postgres', user='postgres', password='postgres', host='158.160.148.72')
+connection = psycopg2.connect(dbname='postgres', user='postgres', password='postgres', host='158.160.152.61')
 cur = connection.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS users(name varchar(100), date_of_bd date, user_id varchar(100), phone varchar(100), email varchar(100), information varchar(200))')
+cur.execute('CREATE TABLE IF NOT EXISTS public.users(name varchar(100), date_of_bd date, chat_id varchar(100), phone varchar(100), email varchar(100), information varchar(200))')
 cur.close()
 connection.close()
 @bot.message_handler(commands=['start','hello'])
 def start(message):
-    global user_id_tg
-    user_id_tg = message.from_user.id
+    global chat_id_tg
+    chat_id_tg = message.chat.id
     bot.send_message(message.chat.id,'Здравствуйте, повелитель!')
     global message_chat_id
     message_chat_id = message.chat.id
     action(message_chat_id)
+def schedule_checker():
+    while True:
+        schedule.run_pending()
+        sleep(1)
+def function_to_run():
+    connection = psycopg2.connect(dbname='postgres', user='postgres', password='postgres',
+                                  host='158.160.152.61')
+    cur = connection.cursor()
+    cur.execute("SELECT name, chat_id FROM public.users where extract(day from date_of_bd) = extract(day from current_timestamp) and extract(month from date_of_bd) = extract(month from current_timestamp)")
+    users = cur.fetchall()
+    for user in users:
+        bot.send_message(user[1], 'Не забудьте поздравить этого человека с др:' + user[0])
+    cur.close()
+    connection.close()
+if __name__ == "__main__":
+    schedule.every().day.at("09:00").do(function_to_run)
+    Thread(target=schedule_checker).start()
 def action(message_chat_id):
     markup = types.InlineKeyboardMarkup()
     btn1 = types.InlineKeyboardButton("Внесите ДР", callback_data='add bd')
@@ -39,6 +58,25 @@ def action(message_chat_id):
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
     if callback.data == 'change_info':
+        connection = psycopg2.connect(dbname='postgres', user='postgres', password='postgres',
+                                      host='158.160.152.61')
+        cur = connection.cursor()
+        message_chat_id = callback.message.chat.id
+        cur.execute("SELECT name FROM public.users where chat_id ='%s' " % (chat_id_tg))
+        users = cur.fetchall()
+        for user in users:
+            x = ' '
+            for name in user:
+                x = name
+                markup = types.InlineKeyboardMarkup()
+                btn_change = types.InlineKeyboardButton(x, callback_data='change_info')
+                markup.add(btn_change)
+                bot.send_message(callback.message.chat.id, reply_markup=markup)
+        cur.close()
+        connection.close()
+        action(message_chat_id)
+        cur.execute("SELECT name') FROM public.users where chat_id ='%s' " % (chat_id_tg))
+        ''' '#вывод кнопок после выбранного юзера
         markup = types.InlineKeyboardMarkup()
         btn4 = types.InlineKeyboardButton("Имя и фамилия", callback_data='name_surname')
         btn5 = types.InlineKeyboardButton("Дата рождения", callback_data='date_bd')
@@ -46,7 +84,8 @@ def callback_message(callback):
         btn7 = types.InlineKeyboardButton("Почта", callback_data='email')
         btn8 = types.InlineKeyboardButton("Информация", callback_data='info')
         markup.add(btn4, btn5, btn6, btn7, btn8)
-        bot.send_message(message_chat_id, 'Что вы хотите изменить?', reply_markup=markup)
+        bot.send_message(callback.message.chat.id, 'Что вы хотите изменить?', reply_markup=markup)
+        '''
     if callback.data == 'add bd':
         bot.send_message(callback.message.chat.id, 'Введите дату в формате дд.мм.гггг')
         @bot.message_handler(content_types=['text'])
@@ -78,12 +117,12 @@ def callback_message(callback):
             global email
             information = message.text.strip()
             connection = psycopg2.connect(dbname='postgres', user='postgres', password='postgres',
-                                          host='158.160.148.72')
+                                          host='158.160.152.61')
             cur = connection.cursor()
-            user_id_tg = message.from_user.id
+            chat_id_tg = message.chat.id
             message_chat_id = message.chat.id
             #print(name, date_of_bd, user_id_tg, phone, email, information)
-            cur.execute("INSERT INTO users(name, date_of_bd, user_id, phone, email, information) VALUES ('%s', to_date('%s','dd.mm.yyyy'), '%s', '%s', '%s', '%s')" % (name, date_of_bd, user_id_tg, phone, email, information))
+            cur.execute("INSERT INTO public.users(name, date_of_bd, chat_id, phone, email, information) VALUES ('%s', to_date('%s','dd.mm.yyyy'), '%s', '%s', '%s', '%s')" % (name, date_of_bd, chat_id_tg, phone, email, information))
             connection.commit()
             cur.close()
             connection.close()
@@ -95,16 +134,13 @@ def callback_message(callback):
 
     if callback.data == 'show bd':
         connection = psycopg2.connect(dbname='postgres', user='postgres', password='postgres',
-                                      host='158.160.148.72')
+                                      host='158.160.152.61')
         cur = connection.cursor()
-        global user_id_tg
         #@todo при падении бота переменые чистятся, и идёт в базу неправильный запрос
         #user_id_tg = callback.message.from_user.id
-
-        global message_chat_id
         message_chat_id = callback.message.chat.id
 
-        cur.execute("SELECT name, to_char(date_of_bd,'dd.mm.yyyy') FROM users where user_id ='%s' " % (user_id_tg))
+        cur.execute("SELECT name, to_char(date_of_bd,'dd.mm.yyyy') FROM public.users where chat_id ='%s' " % (chat_id_tg))
         users = cur.fetchall()
 
         # Выводим результаты
