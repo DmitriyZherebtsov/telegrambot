f0a88d0ea3ac7658655b0ab5b8517907a21f9839
Polly <polinayusupova08@gmail.com>
2024-12-08 13:10:10 +0300

    Настройка логирования

--- handlers_def.py
+++ handlers_def.py
@@ -4,9 +4,13 @@ import telebot
 from telebot import types
 import psycopg2
 import config
+import logging
+logging.basicConfig(level=logging.INFO, filename=config.log_path, filemode="w",
+                    format="%(asctime)s %(levelname)s %(message)s")
 bot = telebot.TeleBot(config.telebot_token)
 
 def menu(message_chat_id):
+    logging.debug("start func 'menu'")
     markup = types.InlineKeyboardMarkup()
     btn1 = types.InlineKeyboardButton("Внесите ДР", callback_data='add bd')
     btn2 = types.InlineKeyboardButton("Показать список ДР", callback_data='show bd')
@@ -15,27 +19,31 @@ def menu(message_chat_id):
     btn_review = types.InlineKeyboardButton("Оставить отзыв", callback_data='review')
     markup.add(btn1).add(btn2).add(btn3).add(btntime).add(btn_review)
     bot.send_message(message_chat_id, 'Выберите действие:', reply_markup=markup)
-def action(message: types.Message):
-    menu(message.chat.id)
 def add_feedback(message):
+    logging.debug("start func 'add_feedback'")
     try:
+        logging.info("connect to db ('add_feedback')")
         connection = psycopg2.connect(dbname = config.db_name, user= config.db_user)
         cur = connection.cursor()
         cur.execute("INSERT INTO memento.review(chat_id, review) VALUES ('%s', '%s')" % (
         message.chat.id, message.text.strip()))
+        logging.info("inserting users' review ('add_feedback')")
         connection.commit()
         cur.close()
         connection.close()
         bot.send_message(message.chat.id, 'Спасибо за Ваш отзыв!')
-        action(message)
+        menu(message.chat.id)
     except Exception:
+        logging.error("smth went wrong", exc_info=True)
         bot.send_message(message.chat.id, 'Что-то пошло не так:(')
-        action(message)
+        menu(message.chat.id)
 
 def change_time(message):
             #TODO: проверить формат времени (:)
+    logging.debug("start func 'chande_time'")
     try:
         time.strptime(message.text.strip(), '%H:%M')
+        logging.info("connect to db ('change_time')")
         connection = psycopg2.connect(dbname = config.db_name, user= config.db_user)
         cur = connection.cursor()
         cur.execute("UPDATE memento.send_time SET update_dt = current_timestamp, time = '%s'  WHERE chat_id ='%s' " % (
@@ -44,8 +52,10 @@ def change_time(message):
         cur.close()
         connection.close()
         bot.send_message(message.chat.id, 'Время изменено успешно')
-        action(message)
+        logging.info("time was changed successfully ('change_time')")
+        menu(message.chat.id)
     except ValueError:
+        logging.error("wrong data or format", exc_info=True)
         bot.send_message(message.chat.id, 'Что-то пошло не так. Попробуйте другие данные.')
-        action(message)
+        menu(message.chat.id)
 
